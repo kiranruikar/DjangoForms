853b4ffce2517094c6347250873065bfc766a3b9
Kiran Ruikar <kiranruikar91@gmail.com>
2021-07-14 22:04:16 +0530

    Submit form

--- src/views.py
+++ src/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import SearchForm
+
 
 # Create your views here.
+
+
+def index(request):
+    form = SearchForm()
+    return render(request, 'index.html', {'form': form})
